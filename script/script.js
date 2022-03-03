5955344e280dbc1099fc1a87eb495161d7d27029
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-03 16:59:42 +0100

    generati numeri random con controllo

--- script/script.js
+++ script/script.js
@@ -10,8 +10,29 @@ playButton.addEventListener('click', play);
 
 
 function play(){
+
     /*PULISCO AD OGNI CLICK IL CONTAINER*/
     container.innerHTML='';
+
+    /*DICHIARO ARRAY PER NUMERI CASUALI*/
+    let bombArray = [];
+
+    for(let n=1; n<=16; n++){
+        /*GENERO IL PRIMO NUMERO FUORI DAL CONTROLLO PERCHE' TANTO ALL'INIZIO L'ARRAY E' VUOTO*/
+        let bombNumber = (Math.floor(Math.random() * (16-0) )+1);
+
+        /*ORA GLI STO DICENDO DI ENTRARE NEL CICLO WHILE SOLO SE IL NUMERO RANDOM E' GIA INCLUSO NELL'Array, COSI FACENDO EVITO I NUMERI DOPPI*/
+        while (bombArray.includes(bombNumber)){
+            bombNumber = bombNumber = (Math.floor(Math.random() * (16 - 1) )+1);
+        }
+        /*ALLA FINE DEL CICLO POSSO AGGIUNGERE IL VALORE ALL'ARRAY*/
+        bombArray.push(bombNumber);
+
+        console.log(bombArray)
+    }
+
+
+   
 
     /*VALUE TIPO DI DIFFICOLTA' SELEZIONATA (0 , 1 , 2) */
     let diffValue = parseInt(selectedElement.value);
@@ -25,7 +46,6 @@ function play(){
     for (let i=1; i<=numCell; i++){
         /*VARIABILE/ELEMENTO DIV*/
         const varElement = document.createElement(`div`);
-        // varElement.innerHTML = '';
 
         /*AGGIUNGO LA CLASSE ALLA VARIBILE/ELEMENTO*/
         varElement.classList.add('box');
@@ -50,31 +70,25 @@ function play(){
         varElement.addEventListener('click', changeColor);
     
         function changeColor() {
-            this.classList.add(`bomb`);
+            this.classList.add(`selected`);
         }
-    
+        
+        /*-GENERARE 16 NUMERI CASUALI IN OGNI RANGE DI DIFFICOLTA, I NUMERI NON POSSONO ESSERE DUPLICATI*/
+            /*
+            -CREO UN ARRAY CON MATH RANDOM CON I NUMERI CASUALI
+            -CON INCLUDES CONTROLLO CHE IL NUMERO NON SIA GIA' USCITO
+            
+            */
+
+        /* SE IL NUMERO E' PRESENTE NELLA LISTA DEI NUMERI GENERATI CASUALMENTE E L'UTENTE CLICCA SU QUELLA CASELLA LA CASELLA DIVENTA ROSSA E LA PARTITA TERMINA*/
+
+        /*SE L'UTENTE VINCE LA PARTITA FINISCE COMUNQUE IL GIOCO*/
+
+        /*COMUNICARE IL PUNTEGGIO ALLA FINE DI OGNI PARTITA */
+       
+
+        /*BONUS:
+        1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
+        2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste*/
     }
 }
-
-
-    // if (selectDifficult == "0"){
-    // container.innerHTML = '';        
-    // for (i=1; i<=49; i++) {
-    //         container.style.width ='420px';
-           
-    //         container.innerHTML += `<div class= "box flex"> ${i}</div>`
-    //     }
-    // } else if(selectDifficult == "1"){
-    //     container.innerHTML ='';
-    //     for (i=1; i<=81; i++) {
-    //         container.style.width ='540px';
-    //         container.innerHTML += `<div class= "box flex">${i}</div>`
-    //     }
-    // }
-    // else if (selectDifficult == "2"){
-    //     container.innerHTML ='';
-    //     for (i=1; i<=100; i++) {
-    //         container.style.width ='600px';
-    //         container.innerHTML += `<div class= "box flex">${i}</div>`
-    //     }
-    // }
