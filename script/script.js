b76c700ce83af76320653a89133503b19d814598
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-03 16:17:37 +0100

    accese luci rosse al click (non riesco a dare l'hover, ho letto che da js è piu complicato)

--- script/script.js
+++ script/script.js
@@ -10,6 +10,8 @@ playButton.addEventListener('click', play);
 
 
 function play(){
+    /*PULISCO AD OGNI CLICK IL CONTAINER*/
+    container.innerHTML='';
 
     /*VALUE TIPO DI DIFFICOLTA' SELEZIONATA (0 , 1 , 2) */
     let diffValue = parseInt(selectedElement.value);
@@ -20,10 +22,10 @@ function play(){
     /* NUMERO DI CELLE PER LATO DEL CONTAINER */
     let cellsSide = Math.sqrt(numCell);
 
-
     for (let i=1; i<=numCell; i++){
         /*VARIABILE/ELEMENTO DIV*/
         const varElement = document.createElement(`div`);
+        // varElement.innerHTML = '';
 
         /*AGGIUNGO LA CLASSE ALLA VARIBILE/ELEMENTO*/
         varElement.classList.add('box');
@@ -31,17 +33,28 @@ function play(){
         /*STAMPO DENTRO LA VARIABILE/ELEMENTO IL NUMERO DELLE CELLE QUINDI IL NUMERO DEL CONTATORE*/
         varElement.append(i);
 
-        /* DIMENSIONO LE CELLE DIVIDENDOLE PER LA RADICE QUADRATA DEL NUMERO TOT DI ELEMENTI, COSI FACENDO TROVO QUANTI CE NE SONO IN UN LATO */ 
+        /* DIMENSIONO LE CELLE DIVIDENDOLE PER LA RADICE QUADRATA DEL NUMERO TOT DI ELEMENTI, COSI FACENDO TROVO QUANTI CE NE SONO IN UN LATO , FLEXO VARELEMENT COSI DA CENTRARE LA I*/ 
+        varElement.style.display='flex';
+        varElement.style.alignItems='center';
+        varElement.style.justifyContent='center';
         varElement.style.width =`calc(100% / ${cellsSide})`;
         varElement.style.height =`calc(100% / ${cellsSide})`;
+        // varElement.style.hover = `color=blue`;
+        // NON SO COME DARE HOVER DA JS 
 
         /*DENTRO IL CONTAINER STAMPO LA VARIABILE ELEMENTO, QUINDI TUTTI I DIV*/ 
         container.append(varElement);
 
+        
         /*FUNZIONE CHE FA COLORARE AL CLICK LE CASELLE*/
-        // varElement.addEventListener('click', changeColor());
-
+        varElement.addEventListener('click', changeColor);
+    
+        function changeColor() {
+            this.classList.add(`bomb`);
+        }
+    
     }
+}
 
 
     // if (selectDifficult == "0"){
@@ -65,4 +78,3 @@ function play(){
     //         container.innerHTML += `<div class= "box flex">${i}</div>`
     //     }
     // }
-}
