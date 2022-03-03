fd77bb397528ffcd500d9b9abf63c6cb9e2f457b
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-03 18:28:56 +0100

    Tentativi con if else

--- script/script.js
+++ script/script.js
@@ -21,14 +21,14 @@ let arrCell = [];
     for(let n=1; n<=16; n++){
 
     /*GENERO IL PRIMO NUMERO FUORI DAL CONTROLLO PERCHE' TANTO ALL'INIZIO L'ARRAY E' VUOTO*/
-    let bombNumber = (Math.floor(Math.random() * (16 - 0) )+1);
+    let bombNumber = parseInt(Math.floor(Math.random() * (16 - 0) )+1);
 
     /*ORA GLI STO DICENDO DI ENTRARE NEL CICLO WHILE SOLO SE IL NUMERO RANDOM E' GIA INCLUSO NELL'Array, COSI FACENDO EVITO I NUMERI DOPPI*/
     while (bombArray.includes(bombNumber)){
           bombNumber = (Math.floor(Math.random() * (16 - 0) )+1);
     }
     /*ALLA FINE DEL CICLO POSSO AGGIUNGERE IL VALORE ALL'ARRAY*/
-    bombArray.push(bombNumber);
+    bombArray.push(parseInt(bombNumber));
 
     }
 
@@ -47,14 +47,35 @@ let arrCell = [];
     for (let i=1; i<=numCell; i++){
 
         
-        arrCell.push(i);
+        arrCell.push(parseInt(i));
 
-        if (bombArray[i]= i){
-            varElement = document.createElement(`div`);
-            varElement.classList.add('bomb');
-            varElement.append(i);
-            console.log (bombArray)
-           } else{
+        if (bombArray[i-1] != arrCell[i]){
+      /*VARIABILE/ELEMENTO DIV*/
+      varElement = document.createElement(`div`);
+
+      /*AGGIUNGO LA CLASSE ALLA VARIBILE/ELEMENTO*/
+      varElement.classList.add('bomb');
+      
+      /*STAMPO DENTRO LA VARIABILE/ELEMENTO IL NUMERO DELLE CELLE QUINDI IL NUMERO DEL CONTATORE*/
+      varElement.append(i);
+         
+
+     
+
+      /* DIMENSIONO LE CELLE DIVIDENDOLE PER LA RADICE QUADRATA DEL NUMERO TOT DI ELEMENTI, COSI FACENDO TROVO QUANTI CE NE SONO IN UN LATO , FLEXO VARELEMENT COSI DA CENTRARE LA I*/ 
+      varElement.style.display='flex';
+      varElement.style.alignItems='center';
+      varElement.style.justifyContent='center';
+      varElement.style.width =`calc(100% / ${cellsSide})`;
+      varElement.style.height =`calc(100% / ${cellsSide})`;
+      /*FUNZIONE CHE FA COLORARE AL CLICK LE CASELLE*/
+      varElement.addEventListener('click', changeColorBomb);
+        
+        
+        } else {
+                // console.log ('non ciao')
+           
+        //    } 
              /*VARIABILE/ELEMENTO DIV*/
         varElement = document.createElement(`div`);
 
@@ -63,7 +84,7 @@ let arrCell = [];
         
         /*STAMPO DENTRO LA VARIABILE/ELEMENTO IL NUMERO DELLE CELLE QUINDI IL NUMERO DEL CONTATORE*/
         varElement.append(i);
-           }
+           
 
        
 
@@ -83,23 +104,22 @@ let arrCell = [];
         /*FUNZIONE CHE FA COLORARE AL CLICK LE CASELLE*/
         varElement.addEventListener('click', changeColor);
     
-           
+        }
 
+
+        
         function changeColor() {
             this.classList.add(`selected`);
         }
+    
+
+        function changeColorBomb(){
+            this.classList.add(`bomb`);
+        }
          
        
-        
-        
-        /*-GENERARE 16 NUMERI CASUALI IN OGNI RANGE DI DIFFICOLTA, I NUMERI NON POSSONO ESSERE DUPLICATI*/
-            /*
-            -CREO UN ARRAY CON MATH RANDOM CON I NUMERI CASUALI
-            -CON INCLUDES CONTROLLO CHE IL NUMERO NON SIA GIA' USCITO
-            
-            */
-
         /* SE IL NUMERO E' PRESENTE NELLA LISTA DEI NUMERI GENERATI CASUALMENTE E L'UTENTE CLICCA SU QUELLA CASELLA LA CASELLA DIVENTA ROSSA E LA PARTITA TERMINA*/
+
         // ASSEGNO I VALORI 
 
         /*SE L'UTENTE VINCE LA PARTITA FINISCE COMUNQUE IL GIOCO*/
